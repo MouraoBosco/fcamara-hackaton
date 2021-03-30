4e19356260b5c7baf4df9c4872386115e77b3f6c
MouraoBosco <mateusmouraobosco@hotmail.com>
2021-03-29 22:41:30 -0300

    estruturacao da homepage

---
+++ fcamara-app/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { DoacaoComponent } from './components/doacao/doacao.component';
+import { EscolasComponent } from './components/escolas/escolas.component';
+import { MainComponent } from './components/main/main.component';
+
+const routes: Routes = [
+  {path:'main', component: MainComponent},
+  {path:'about', component: AboutComponent},
+  {path:'cadastro', component: CadastroComponent},
+  {path:'escolas', component: EscolasComponent},
+  {path:'doacao', component: DoacaoComponent},
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
