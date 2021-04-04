5e985acf3344d11d3343b181919fde199ddbb5e1
ThiagoMoura963 <thiagomjfelis3@gmail.com>
2021-04-03 22:48:11 -0300

    Implementação do módulo de Flex-Layout para deixar o site responsivo

--- fcamara-app/src/app/components/cadastro/cadastro.component.spec.ts
+++ fcamara-app/src/app/components/cadastro/cadastro.component.spec.ts
@@ -2,6 +2,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { CadastroComponent } from './cadastro.component';
 
